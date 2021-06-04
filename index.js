4f330d6cc9649e44bea4ec39a5109a0ea1faf1a5
Emmett Rushin <emmettrushin@gmail.com>
2021-06-04 15:15:19 -0400

    wrote if else statement for whether they want to add an employee or not

--- index.js
+++ index.js
@@ -31,66 +31,85 @@ employeeType = () => {
 };
 
 addEngineer = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the Engineer's name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Engineer's employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Engineer's email address:",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Engineer's github user name:",
-            name: "github"
-        }
-    ]).then((engineerResults) => {
-        engineerResults.role = "Engineer";
-        const { name, id, email, github, role } = engineerResults;
-        const newEngineer = new Engineer(name, id, email, github, role);
-        employees.push(newEngineer);
-        addEmployee();
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Engineer's employee ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Engineer's email address:",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Engineer's github user name:",
+        name: "github",
+      },
+    ])
+    .then((engineerResults) => {
+      engineerResults.role = "Engineer";
+      const { name, id, email, github, role } = engineerResults;
+      const newEngineer = new Engineer(name, id, email, github, role);
+      employees.push(newEngineer);
+      addEmployee();
     });
 };
 
-
 addIntern = () => {
-    return inquirer.prompt([
-        //question about Intern
-        {
-            type: "input",
-            message: "What is the Intern's name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Intern's employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Intern's email address:",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Intern's school:",
-            name: "school"
-        }
-    ]).then((internResults) => {
-        internResults.role = "Intern";
-        const { name, id, email, school, role } = internResults;
-        const newIntern = new Intern(name, id, email, school, role);
-        employees.push(newIntern);
-        //ask if user wants to add another team member
-        addEmployee();
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Intern's employee ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Intern's email address:",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Intern's school:",
+        name: "school",
+      },
+    ])
+    .then((internResults) => {
+      internResults.role = "Intern";
+      const { name, id, email, school, role } = internResults;
+      const newIntern = new Intern(name, id, email, school, role);
+      employees.push(newIntern);
+      addEmployee();
+    });
+};
+
+addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Add another team member?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((choice) => {
+      if (choice.add === "yes") {
+        employeeType();
+      } else {
+        html();
+      }
     });
 };
