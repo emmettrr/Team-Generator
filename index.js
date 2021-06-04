eb77c6bafac9bcbbf99caf1bc96e8538940d1bf1
Emmett Rushin <emmettrushin@gmail.com>
2021-06-04 15:10:18 -0400

    added inquirer prompt for engineer

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const inquirer = require("inquirer");
 const path = require("path");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/html");
+const manager = require("./lib/manager");
+const employee = require("./lib/employee");
+const intern = require("./lib/intern");
+const engineer = require("./lib/engineer");
 const employees = [];
 
-employees = () => {
+employeeType = () => {
   console.log("What is the employee's role?");
   return inquirer
     .prompt([
@@ -23,5 +27,38 @@ employees = () => {
       } else {
         addIntern();
       }
+    });
+};
+
+addEngineer = () => {
+    return inquirer.prompt([
+        //question about Engineer
+        {
+            type: "input",
+            message: "What is the Engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Engineer's employee ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Engineer's email address:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Engineer's github user name:",
+            name: "github"
+        }
+    ]).then((engineerResults) => {
+        engineerResults.role = "Engineer";
+        const { name, id, email, github, role } = engineerResults;
+        const newEngineer = new Engineer(name, id, email, github, role);
+        employees.push(newEngineer);
+        //ask if user wants to add another team member
+        addEmployee();
     });
 };
