55323ec6691657e19f48ac010232de0551884089
Emmett Rushin <emmettrushin@gmail.com>
2021-06-08 11:10:33 -0400

    removed created html and fixed html.js

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,3 +1,19 @@
 const Engineer = require('../lib/engineer')
 
-test("creates an engineer ")
+test("creates an engineer object", () => {
+    const engineer = new Engineer("Emmett", 30, "fakeemail@gmail.com", "emmettrr")
+
+    expect(engineer.github).toEqual(expect.any(String))
+})
+
+test("gets engineer github value", () => {
+    const engineer = new Engineer("Emmett", 30, "fakeemail@gmail.com", "emmettrr")
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+})
+
+test("gets role of employee", () => {
+    const engineer = new Engineer("Emmett", 30, "fakeemail@gmail.com", "emmettrr")
+
+    expect(engineer.getRole()).toEqual("Engineer")
+})
