41770c4a522830326bc56f449d37e6c0e0d1d1b2
Emmett Rushin <emmettrushin@gmail.com>
2021-06-08 11:18:35 -0400

    added intern test

---
+++ __tests__/intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/intern')
+
+test("creates an intern object", () => {
+    const intern = new Intern('Emmett', 30, 'fakeemail@gmail.com', 'Landmark College')
+
+    expect(inter.school).toEqual(expect.any(String))
+})
+
+test("gets intern's school", () => {
+    const intern = new Intern('Emmett', 30, 'fakeemail@gmail.com', 'Landmark College')
+
+    expect(intern.school).toEqual(expect.stringContaining(intern.school.toString()))
+})
+
+test("gets role of employee", () => {
+    const intern = new Intern('Emmett', 30, 'fakeemail@gmail.com', 'Landmark College')
+
+    expect(intern.getRole()).toEqual("Intern")
+})
